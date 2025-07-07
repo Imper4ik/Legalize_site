aa55a1d7397ec53b41b573ce3498d81dd30a6cab
Butouski Mikita <nindse@gmail.com>
2025-07-07 22:56:52 +0200

    small fix

---
+++ legalize_site/clients/templatetags/form_filters.py
@@ -0,0 +1,17 @@
+# clients/templatetags/form_filters.py
+
+from django import template
+
+register = template.Library()
+
+@register.filter(name='add_attr')
+def add_attr(field, css):
+    attrs = {}
+    definition = css.split(',')
+
+    for d in definition:
+        if ':' in d:
+            key, val = d.split(':')
+            attrs[key] = val
+
+    return field.as_widget(attrs=attrs)
