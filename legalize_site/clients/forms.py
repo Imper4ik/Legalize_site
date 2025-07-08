443f7740bddfc223f41eccd2cf5c075b1498e78d
Butouski Mikita <nindse@gmail.com>
2025-07-08 22:18:56 +0200

    add update

--- legalize_site/clients/forms.py
+++ legalize_site/clients/forms.py
@@ -4,29 +4,19 @@ from django import forms
 from .models import Client, Document
 
 class ClientForm(forms.ModelForm):
-    # Явно переопределяем поле для даты
-    legal_basis_end_date = forms.DateField(
-        label="Дата окончания основания",
-        required=False,
-        input_formats=['%d.%m.%Y'],
-        # ИЗМЕНЕНИЕ ЗДЕСЬ: добавляем все нужные атрибуты прямо в виджет
-        widget=forms.TextInput(attrs={
-            'placeholder': 'дд.мм.гггг',
-            'data-input': ''  # Атрибут, который ищет flatpickr
-        })
-    )
-
     class Meta:
         model = Client
-        fields = '__all__'
+        fields = '__all__' # Просто используем все поля из модели
 
-    # Этот метод добавляет стили Bootstrap ко всем полям
+    # Этот метод просто добавляет стили Bootstrap
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
-            field.widget.attrs.update({'class': 'form-control'})
+            # Пропускаем поле с датой, так как мы его стилизуем вручную
+            if field_name != 'legal_basis_end_date':
+                field.widget.attrs.update({'class': 'form-control'})
 
-# Эта форма нужна для загрузки файлов
+# Эта форма для загрузки файлов
 class DocumentUploadForm(forms.ModelForm):
     class Meta:
         model = Document
