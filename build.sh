4aa419ef68b3394b6f9a0bf028b302293baa852f
Butouski Mikita <nindse@gmail.com>
2025-07-28 18:36:24 +0200

    new upd

--- build.sh
+++ build.sh
@@ -2,7 +2,15 @@
 # exit on error
 set -o errexit
 
+# Обновляем сам установщик, чтобы избежать проблем
+pip install --upgrade pip
+
+# Принудительно удаляем старые версии перед установкой
+pip uninstall -y django-sendgrid-v5 sendgrid
+
+# Устанавливаем все пакеты заново из вашего requirements.txt
 pip install -r requirements.txt
 
+# Ваши стандартные команды
 python manage.py collectstatic --no-input
 python manage.py migrate
