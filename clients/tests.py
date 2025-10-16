de8200b0593b81f8befaec7e325f66c08d1fb76b
Butouski Mikita <141655795+Imper4ik@users.noreply.github.com>
2025-10-16 18:50:41 +0200

    Add language switcher to login toolbar

--- clients/tests.py
+++ clients/tests.py
@@ -1,3 +1,39 @@
-from django.test import TestCase
+from datetime import datetime, timedelta
 
-# Create your tests here.
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+from django.utils import translation
+
+
+class CalculatorViewTests(TestCase):
+    def setUp(self):
+        user_model = get_user_model()
+        self.staff_user = user_model.objects.create_user(
+            username='staff', password='pass', is_staff=True
+        )
+
+    def test_months_in_period_multiplies_tuition_total(self):
+        login_successful = self.client.login(username='staff', password='pass')
+        self.assertTrue(login_successful)
+
+        future_date = (datetime.now() + timedelta(days=30)).strftime('%d-%m-%Y')
+        with translation.override('ru'):
+            url = reverse('clients:calculator')
+
+        response = self.client.post(url, {
+            'total_end_date': future_date,
+            'tuition_fee': '1000',
+            'tuition_currency': 'PLN',
+            'months_in_period': '6',
+            'rent_and_bills': '0',
+            'rent_currency': 'PLN',
+            'num_people': '1',
+            'fee_type': 'per_month',
+        })
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('results', response.context)
+        results = response.context['results']
+        self.assertEqual(results['monthly_tuition_calculated'], '1 000.00')
+        self.assertEqual(results['tuition_total'], '6 000.00')
