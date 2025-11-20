8213f923096742dfe2743ad58a6e26766b79571a
Butouski Mikita <141655795+Imper4ik@users.noreply.github.com>
2025-11-20 21:28:42 +0100

    Centralize env loading and update deployment config

--- legalize_site/asgi.py
+++ legalize_site/asgi.py
@@ -11,6 +11,9 @@ import os
 
 from django.core.asgi import get_asgi_application
 
+from legalize_site.env import load_env
+
+load_env()
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'legalize_site.settings')
 
 application = get_asgi_application()
--- start.sh
+++ start.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# 1. Собрать статические файлы
-python manage.py collectstatic --no-input  # <--- КРИТИЧЕСКОЕ ДОБАВЛЕНИЕ
-
-# 2. Выполнить миграции
 python manage.py migrate --no-input
 
-# 3. Запустить Gunicorn
 : "${PORT:=8000}"
 exec gunicorn legalize_site.wsgi:application --bind 0.0.0.0:"${PORT}"
