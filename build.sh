508f605c7c891c20129853364d469ef4ccd80bd6
Butouski Mikita <141655795+Imper4ik@users.noreply.github.com>
2025-11-20 22:37:45 +0100

    Install gettext during build

--- build.sh
+++ build.sh
@@ -2,6 +2,11 @@
 # exit on error
 set -o errexit
 
+if ! command -v msgfmt >/dev/null 2>&1; then
+  apt-get update
+  apt-get install -y gettext
+fi
+
 pip install --upgrade pip
 pip install -r requirements.txt
 
